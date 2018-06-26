0296f487f769c3d106a2b102368b39af5cc3b06c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-06-26 21:18:45 +0100

    Draw board of tiles to screen

--- main.lua
+++ main.lua
@@ -5,6 +5,10 @@ function love.load()
 
     math.randomseed(os.time())
 
+    tileSprite = love.graphics.newImage('assets/sprites/match3.png')
+    tileQuads = GenerateQuads(tileSprite, 32, 32)
+    board = generateBoard()
+
     Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
@@ -38,6 +42,39 @@ end
 
 function love.draw()
     Push:start()
+
+    drawBoard(128, 16)
     
     Push:finish()
+end
+
+function generateBoard()
+    local tiles = {}
+
+    for y = 1, 8 do
+        table.insert(tiles, {})
+        for x = 1, 8 do
+            table.insert(tiles[y], {
+                x = (x - 1) * 32,
+                y = (y - 1) * 32,
+                tile = math.random(#tileQuads)
+            })
+        end
+    end
+
+    return tiles
+end
+
+function drawBoard(offsetX, offsetY)
+    for y = 1, 8 do
+        for x = 1, 8 do
+            local tile = board[y][x]
+            love.graphics.draw(
+                tileSprite, 
+                tileQuads[tile.tile],
+                tile.x + offsetX, 
+                tile.y + offsetY
+            )
+        end
+    end
 end
--- src/utils/dependencies.lua
+++ src/utils/dependencies.lua
@@ -1,2 +1,3 @@
+require('src.utils.utils')
 require('src.utils.constants')
 Push = require('src.lib.push')
