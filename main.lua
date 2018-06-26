9853d5dc60bc8d30beba598ac743814b2fab3f5e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-06-26 21:55:38 +0100

    Highlight currently selected tile

--- main.lua
+++ main.lua
@@ -6,8 +6,12 @@ function love.load()
     math.randomseed(os.time())
 
     tileSprite = love.graphics.newImage('assets/sprites/match3.png')
-    tileQuads = GenerateQuads(tileSprite, 32, 32)
+    tileQuads = GenerateQuads(tileSprite, TILE_SIZE, TILE_SIZE)
     board = generateBoard()
+
+    highlightedTile = true
+    highlightedX, highlightedY = 1, 1
+    selectedTile = board[1][1]
 
     Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
@@ -23,10 +27,30 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
-    love.keyboard.keysPressed[key] = true
-
     if key == 'escape' then
         love.event.quit()
+    end
+    
+    love.keyboard.keysPressed[key] = true
+
+    local x, y = selectedTile.gridX, selectedTile.gridY
+    
+    if key == 'up' then
+        if y > 1 then
+            selectedTile = board[y - 1][x]
+        end
+    elseif key == 'down' then
+        if y < 8 then
+            selectedTile = board[y + 1][x]
+        end
+    elseif key == 'left' then
+        if x > 1 then
+            selectedTile = board[y][x - 1]
+        end
+    elseif key == 'right' then
+        if x < 8 then
+            selectedTile = board[y][x + 1]
+        end
     end
 end
 
@@ -55,8 +79,10 @@ function generateBoard()
         table.insert(tiles, {})
         for x = 1, 8 do
             table.insert(tiles[y], {
-                x = (x - 1) * 32,
-                y = (y - 1) * 32,
+                x = (x - 1) * TILE_SIZE,
+                y = (y - 1) * TILE_SIZE,
+                gridX = x,
+                gridY = y,
                 tile = math.random(#tileQuads)
             })
         end
@@ -75,6 +101,10 @@ function drawBoard(offsetX, offsetY)
                 tile.x + offsetX, 
                 tile.y + offsetY
             )
+            love.graphics.setColor(1, 0, 0, 0.95)
+            love.graphics.setLineWidth(4)
+            love.graphics.rectangle('line', selectedTile.x + offsetX, selectedTile.y + offsetY, 32, 32, 4)
+            love.graphics.setColor(1, 1, 1, 1)
         end
     end
 end
